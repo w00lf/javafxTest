b15fcbd36c92779cf29c5a030351252d6950c2e9
MiklT <roberar2ua@gmail.com>
2015-06-17 23:33:27 +0300

    Added seting circles count

--- src/sample/Circles.java
+++ src/sample/Circles.java
@@ -1,17 +1,19 @@
 package sample;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.BlendMode;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
@@ -20,12 +22,11 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import javafx.util.converter.NumberStringConverter;
 
 import java.util.ArrayList;
-
-import static java.lang.Math.random;
+import java.util.Iterator;
+import java.util.Stack;
 
 /**
  * Created by w00lf on 10.06.2015.
@@ -36,6 +37,9 @@ public class Circles {
     private Stage currentStage;
     private Scene currentScene;
     private TextField radiusTextField;
+    private TextField circlesCountTextField;
+    private final Group circles = new Group();
+    private String currentColor = "white";
 
 
     public void start(Stage stage){
@@ -46,16 +50,45 @@ public class Circles {
         radiusTextField = new TextField();
         currentScene = new Scene(root, 800, 600, Color.BLACK);
         Rectangle colors = getGradient(currentScene);
-        Group circles = getWhiteCircles();
+        int initialCirclesCount = 20;
+        Group circles = produceCircles(initialCirclesCount, currentColor);
 
         Group blendModeGroup =
                 new Group(new Group(new Rectangle(currentScene.getWidth(), currentScene.getHeight(),
                         Color.BLACK), circles), colors);
         colors.setBlendMode(BlendMode.OVERLAY);
+
+        circlesCountTextField = new TextField();
+        circlesCountTextField.setText(String.valueOf(initialCirclesCount));
+        circlesCountTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable,
+                                String oldValue, String newValue) {
+                circlesCountTextField.setDisable(true);
+                handleCircles(Integer.parseInt(newValue));
+                circlesCountTextField.setDisable(false);
+            }
+        });
+        circlesCountTextField.setAlignment(Pos.TOP_RIGHT);
+        radiusTextField.setAlignment(Pos.BOTTOM_RIGHT);
         root.getChildren().add(blendModeGroup);
-        root.getChildren().add(radiusTextField);
+        GridPane grid = new GridPane();
+        grid.add(radiusTextField, 0, 0);
+        grid.add(circlesCountTextField, 0, 1);
+        root.getChildren().add(grid);
         prevousScene = currentStage.getScene();
         currentStage.setScene(currentScene);
+    }
+
+    private void handleCircles(int newValue) {
+        if(newValue < 0)
+            return;
+        int circlesCount = circles.getChildren().size();
+        if(circlesCount > newValue){
+            removeCircles(circlesCount - newValue);
+            return;
+        }
+        produceCircles(newValue - circlesCount, currentColor);
     }
 
     private Rectangle getGradient(Scene scene) {
@@ -87,10 +120,9 @@ public class Circles {
         });
     }
 
-    private Group getWhiteCircles() {
-        Group circles = new Group();
-        for (int i = 0; i < 20; i++) {
-            Circle circle = new Circle(150, Color.web("white", 0.05));
+    private Group produceCircles(int count, String color) {
+        for (int i = 0; i < count; i++) {
+            Circle circle = new Circle(150, Color.web(color, 0.05));
             Bindings.bindBidirectional(radiusTextField.textProperty(), circle.radiusProperty(), new NumberStringConverter());
             circle.setStrokeType(StrokeType.OUTSIDE);
             circle.setStroke(Color.web("white", 0.16));
@@ -102,5 +134,11 @@ public class Circles {
         CirclesAnimator circlesAnimator = new CirclesAnimator(myList, currentScene);
         circlesAnimator.animate();
         return circles;
+    }
+
+    private void removeCircles(int count) {
+        int circlesCount = circles.getChildren().size();
+        int circlesToRemove = (count > circlesCount) ? circlesCount : count;
+        circles.getChildren().remove(0, circlesToRemove);
     }
 }
